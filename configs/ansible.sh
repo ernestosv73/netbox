2f4254f66701a4a2242624e44095bb2809b5c703
Ernesto Sánchez <esanchez@ucasal.edu.ar>
2025-12-19 00:38:53 -0300

    Update ansible.sh

--- configs/ansible.sh
+++ configs/ansible.sh
@@ -1,8 +1,23 @@
+echo "crear directorio"
 mkdir ansible-netbox
+sleep 1
+echo "cambiar a directorio!
 cd ansible-netbox
+sleep 1
+echo "crear contenedor"
 python3 -m venv .
+sleep 1
 source bin/activate
+sleep 1
+echo "install pynetbox"
 pip3 install pynetbox
+sleep 2
+echo "install ansible"
 pip install ansible
+sleep 5
+echo "install netaddr"
 pip install netaddr
+sleep 1
+echo "install pytz"
 pip install pytz
+sleep 1
