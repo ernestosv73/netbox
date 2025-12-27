8cddb30efbc2f3814d39457ea080b70013820976
Ernesto Sánchez <esanchez@ucasal.edu.ar>
2025-12-26 23:55:05 -0300

    Update and rename netmiko.sh to nornir.sh

--- configs/netmiko.sh
+++
@@ -1,4 +0,0 @@
-apt install python3-dev build-essential libssl-dev libffi-dev -y
-sleep 3
-pip install netmiko
-sleep 1
---
+++ configs/nornir.sh
@@ -0,0 +1,7 @@
+apt install python3-dev build-essential libssl-dev libffi-dev -y
+sleep 3
+pip install nornir nornir-netmiko nornir-napalm nornir-utils
+sleep 3
+pip install pynetbox
+sleep 1
+
